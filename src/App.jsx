efb5a68636c466480d44664a46bafa4d60e2b33e
Jin2oo2 <nojinhiroaki@icloud.com>
2024-03-05 18:58:42 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Game } from './pages/Game'
+
+function App() {
+
+  return (
+    <>
+      <h1>Chess App</h1>
+      <Game></Game>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Game.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+function Game() {
+  return (
+    <>
+        <h1>This is chess board</h1>
+    </>
+  )
+}
+
+export default Game
