c6bdc360bbe3af217af9e3910f08f30d59af7703
Jin2oo2 <nojinhiroaki@icloud.com>
2024-03-15 17:38:01 +0000

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import { Box } from '@chakra-ui/react'
- import Game from './pages/Game'
+import Game from './pages/Game'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <Box bg='tomato' w='100%' h='100%' p={4} color='white'>
-        Chess App
-        <Game></Game>
+      <Box bgGradient="linear(to-t, #fff2cc, #f5e6d1)" w='100%' h='calc(100vh)'>
+        <Header></Header>
+        
       </Box>
+      
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Box, Button, Text } from '@chakra-ui/react'
+import chess_header_icon from '/chess_header.png'
+
+export default function Header() {
+  return (
+    <>
+        <Box bg="white" boxShadow="md" p={3} display="flex" justifyContent="space-between">
+            <Box as='button' display='flex' alignItems="center">
+                <img src={chess_header_icon} width="40" height="40"/>
+                <Text fontSize="2xl" fontWeight="bold" ml={3}>ChessMaster Online</Text>
+            </Box>
+            
+            <div id='rightside'>
+                <Button mr={6} variant='ghost' colorScheme='whatsapp'>Login</Button>
+                <Button mr={2} variant='outline' colorScheme='whatsapp'>Sign Up</Button>
+            </div>
+        </Box>
+    </>
+  )
+}
