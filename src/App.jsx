dab00a83a3a6adf5dbd5ed51ca267f05015e4018
Jin2oo2 <nojinhiroaki@icloud.com>
2024-03-22 21:52:39 +0000

    done basic routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@chakra-ui/react'
 import Game from './pages/Game'
 import Header from './components/Header'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 import Home from './pages/Home'
+import NoPage from './pages/NoPage';
 
 function App() {
 
@@ -12,8 +14,15 @@ function App() {
     <>
       <Box bgGradient="linear(to-t, #fff2cc, #f5e6d1)" w='100%' h='calc(100vh)'>
         <Header />
-        <Home />
-        
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/chessgame' element={<Game />} />
+            <Route path='/signup' element={<Signup />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='*' element={<NoPage />} />
+          </Routes>
+        </BrowserRouter>
       </Box>
       
     </>
